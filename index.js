b929b5ee3e5fecbb4ea726718ba29995d132b542
RAM-BHARATH <ramtherapid007@gmail.com>
2022-06-11 17:39:55 +0530

    Used express

--- index.js
+++ index.js
@@ -1,74 +1,75 @@
-const http = require('http')
-const axios = require('axios')
-require('dotenv').config()
+const express = require('express');
+const app = express();
 const fs = require('fs');
 
 const port = process.env.PORT || 3000
 
-const pageDir = './src/pages/'
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'})
-  if(req.url==='/'||req.url==='/about'||req.url==='/contact-me'||req.url==="/styles/style.css"){
-    console.log('req.url: ',req.url)
-    if(req.url==='/'){
-      fs.readFile(pageDir+'index.html',(err, data)=>{
-        if(err) throw err;
-        return res.end(data);
-      })
-    }else{
-      if(req.url==="/styles/style.css"){
-        fs.readFile('./src/styles/style.css',(err, data)=>{
-          if(err) throw err;
-          res.writeHead(200, {'Content-Type': 'text/css'})
-          return res.end(data);
-        })        
-      }else{
-        fs.readFile(pageDir+req.url.slice(1)+'.html',(err, data)=>{
-          if(err) throw err;
-          res.writeHead(200, {'Content-Type': 'text/html'})
-          return res.end(data);
-        })
-      }
-    }
-  }
-  else{
-    fs.readFile(pageDir+'404.html', (err, data) =>{
-      if(err) throw err
-      res.writeHead(200, {'Content-Type': 'text/html'})
-      res.end(data)
-    })
-  }
-  // res.end(pageDir+'index.html');
-  // if(req.url==='/'){
-  //   fs.readFileSync(pageDir+'index.html', (err, data)=>{
-  //     if (err) throw err;
-  //     res.writeHead(200, {'Content-Type': 'text/html'})
-  //     res.write(data)
-  //     return res.end
-  //   })
-  // }else if(req.url==='/about'){
-  //   fs.readFileSync(pageDir+'about.html', (err, data)=>{
-  //     if (err) throw err;
-  //     res.writeHead(200, {'Content-Type': 'text/html'})
-  //     res.write(data)
-  //     return res.end;
-  //   })
-  // }else{
-  //   fs.readFileSync(pageDir+'404.html', (err, data)=>{
-  //     if (err) throw err;
-  //     res.writeHead(200, {'Content-Type': 'text/html'})
-  //     res.write(data)
-  //     return res.end;
-  //   })
-  // }
+app.use(function (req,res,next){
+	res.status(404).send('Unable to find the requested resource!');
+});
+
+
+app.get('/',(req, res)=>{
+  // res.send("Hello World!");
+  res.sendFile(__dirname+'/src/pages/index.html')
+})
+
+app.get('/about', (req, res)=>{
+  res.sendFile(__dirname+'/src/pages/about.html');
+})
+
+app.get('/contact-me', (req, res)=>{
+  res.sendFile(__dirname+'/src/pages/contact-me.html');
+})
+
+app.get('/styles/style.css', (req, res)=>{
+  // res.writeHead(200, {'Content-Type':'text/css'})
+  res.sendFile(__dirname+'/src/styles/style.css');
 })
 
 
-// axios
-//     .get('index.html')
-//     .then(res => {
+// const http = require('http')
+// const axios = require('axios')
+// require('dotenv').config()
+// const fs = require('fs');
 
+// const port = process.env.PORT || 3000
+
+// const pageDir = './src/pages/'
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, {'Content-Type': 'text/html'})
+//   if(req.url==='/'||req.url==='/about'||req.url==='/contact-me'||req.url==="/styles/style.css"){
+//     console.log('req.url: ',req.url)
+//     if(req.url==='/'){
+//       fs.readFile(pageDir+'index.html',(err, data)=>{
+//         if(err) throw err;
+//         return res.end(data);
+//       })
+//     }else{
+//       if(req.url==="/styles/style.css"){
+//         fs.readFile('./src/styles/style.css',(err, data)=>{
+//           if(err) throw err;
+//           res.writeHead(200, {'Content-Type': 'text/css'})
+//           return res.end(data);
+//         })        
+//       }else{
+//         fs.readFile(pageDir+req.url.slice(1)+'.html',(err, data)=>{
+//           if(err) throw err;
+//           res.writeHead(200, {'Content-Type': 'text/html'})
+//           return res.end(data);
+//         })
+//       }
+//     }
+//   }
+//   else{
+//     fs.readFile(pageDir+'404.html', (err, data) =>{
+//       if(err) throw err
+//       res.writeHead(200, {'Content-Type': 'text/html'})
+//       res.end(data)
 //     })
-server.listen(port, () => {
+//   }
+// })
+
+app.listen(port, () => {
   console.log(`Server running at port ${port}`)
 })
