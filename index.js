f6a3d0fceb628786b95e76d224ede4e1305808f2
RAM-BHARATH <ramtherapid007@gmail.com>
2022-06-11 19:29:30 +0530

    404 Page

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const port = process.env.PORT || 3000
 
 app.use(function (req,res,next){
-	res.status(404).send('Unable to find the requested resource!');
+	res.status(404).sendFile(__dirname+'/src/pages/404.html');
 });
 
 
